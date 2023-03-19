8f607a3a4025e3970ca59afad6f64e1e5e03a402
tubery <pdinh74@gmail.com>
2023-03-19 19:59:00 +0000

    Styling and Toggle feature, added skeleton styling with description toggling

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("JS Link success!");
+// Selectors
+const buttons = document.querySelectorAll(".card-toggle-description");
+
+// Adding event listeners to buttons
+buttons.forEach((button) => {
+	button.addEventListener("click", (e) => {
+		e.preventDefault();
+		// console.log(e.target.id);
+		console.log(document.querySelector(`#paragraph-${e.target.id}`));
+		document
+			.querySelector(`#paragraph-${e.target.id}`)
+			.classList.toggle("show");
+	});
+});
